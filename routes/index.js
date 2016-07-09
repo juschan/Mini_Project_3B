2a82088a714c60b566bfdc2325648f49cf9dbcab
Justin Chan <jcsl76@yahoo.com>
2016-07-09 07:52:29 +0100

    Feature Complete: 'Get /profile' - Get profile statement

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 var summary = {
   name: "Justin Chan",
-  tagline: "Software Craftsman"
+  tagline: "Software Craftsman",
+  statement: "Crafting elegant solutions"
 }
 
-/* GET home page. */
+/* GET / - default */
 router.get('/', function(req, res, next) {
   res.status(200).json({summary});
+});
+
+/* GET profile statement */
+router.get('/profile', function(req, res, next) {
+  res.status(200).json( { summary: {statement: summary.statement} });
 });
 
 module.exports = router;
--- test/index_test.js
+++ test/index_test.js
@@ -16,13 +16,36 @@ describe('GET /', () => {
         .expect(200,done);
     }); //end it
 
-    //test to return summary with name and tagline
+    //expect response to return summary with name and tagline
     it('should return a summary with name and tagline', (done) => {
         api.get('/')
         .set('Accept', 'application.json')
         .end((end, response) => {
             expect(response.body.summary).to.have.property('name');
             expect(response.body.summary).to.have.property('tagline');
+            done();
+        }); //end api.end
+    }); //end it
+
+}); //end describe
+
+
+//test to get profile statement
+describe('GET /profile', () => {
+
+    //test to return 200 response
+    it('should return a 200 response', (done) => {
+        api.get('/profile')
+        .set('Accept', 'application.json')
+        .expect(200,done);
+    }); //end it
+
+    //expect response to return profle statement
+    it('should return a profile statement', (done) => {
+        api.get('/profile')
+        .set('Accept', 'application.json')
+        .end((end, response) => {
+            expect(response.body.summary).to.have.property('statement');
             done();
         }); //end api.end
     }); //end it
