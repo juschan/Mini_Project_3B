7121c16fa780cc61ebc8aeae853bca20c2bcedcb
Justin Chan <jcsl76@yahoo.com>
2016-08-01 06:59:34 +0100

    Added GET routes. GET /all working

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,36 @@ router.get('/', function(req, res, next) {
   res.render('../views/index.ejs')
 });
 
+// GET /summary - shows summary 
+router.get('/all', function(req, res, next) {
+  Oneself.find({}, function(err, oneself) {
+    res.status(200).json( {oneself})
+  });
+});
 
 // GET /summary - shows summary 
 router.get('/summary', function(req, res, next) {
-  res.status(200).json( {summary: "text"});
+  Oneself.find({}, function(err, oneself) {
+    var summary = oneself.summary
+    res.status(200).json( {summary})
+  });
 });
+
 
 // GET /links
 router.get('/links', function(req, res, next) {
-  res.status(200).json( {links} );
+  Oneself.find({}, function(err, oneself) {
+    var links = oneself.links
+    res.status(200).json({links})
+  });
 });
 
 // GET projects
 router.get('/projects', function(req, res, next) {
-  res.status(200).json( {projects} );
+  Oneself.find({}, function(err, oneself) {
+    var projects = oneself.projects
+    res.status(200).json({projects})
+  });
 });
 
 
@@ -38,12 +54,12 @@ router.get('/projects/:id', function(req, res, next) {
   }
 });
 
-
 // GET skills
 router.get('/skills', function(req, res, next) {
-  res.status(200).json( {skills});
+  Oneself.find({}, function(err, oneself) {
+    var skills = oneself.skills
+    res.status(200).json({skills})
+  });
 });
-
-
 
 module.exports = router;
